04a349b4625e67dc3a0cca47bb1a65828a04e3ea
Xiong-Da <465981796@qq.com>
2017-07-30 15:50:17 +0800

    a big progress

--- agent.py
+++ agent.py
@@ -12,15 +12,15 @@ input_state=tf.placeholder(tf.float32,[None,1+1+3*3])
 #[flyActionValue,notFlyActionValue]
 input_value=tf.placeholder(tf.float32,[None])
 
-weight1 = tf.Variable(tf.truncated_normal([1+1+3*3,64], stddev=0.1))
-bias1 = tf.Variable(tf.zeros([64]))
+weight1 = tf.Variable(tf.truncated_normal([1+1+3*3,128], stddev=0.1))
+bias1 = tf.Variable(tf.zeros([128]))
 output1=tf.nn.relu(tf.matmul(input_state,weight1)+bias1)
 
-weight2 = tf.Variable(tf.truncated_normal([64,64], stddev=0.1))
-bias2 = tf.Variable(tf.zeros([64]))
+weight2 = tf.Variable(tf.truncated_normal([128,128], stddev=0.1))
+bias2 = tf.Variable(tf.zeros([128]))
 output2=tf.nn.relu(tf.matmul(output1,weight2)+bias2)
 
-weight3 = tf.Variable(tf.truncated_normal([64,2], stddev=0.1))
+weight3 = tf.Variable(tf.truncated_normal([128,2], stddev=0.1))
 bias3 = tf.Variable(tf.zeros([2]))
 output3=tf.matmul(output2,weight3)+bias3
 
@@ -32,14 +32,14 @@ output_value=tf.reduce_sum(tf.multiply(value_array,input_action),axis=1)
 diff_vector=input_value-output_value
 
 loss=tf.reduce_mean(tf.square(diff_vector))
-train_op=tf.train.AdamOptimizer(0.00002).minimize(loss)
+train_op=tf.train.AdamOptimizer(0.0005).minimize(loss)
 
 initer=tf.global_variables_initializer()
 saver=tf.train.Saver()
 
 ##############################################################
 
-def addTubeData(data,birdPos,tube):
+def addTubeData(data,tube):
     data.append(tube[0])
     data.append(tube[1])
     data.append(tube[2]-tube[1])
@@ -68,13 +68,13 @@ def translateState(rawSate):
         retList.append(0)
         retList.append(maxTubeGap)
     else:
-        addTubeData(retList,birdPos,tubes[index-1])
-    addTubeData(retList,birdPos,tubes[index])
+        addTubeData(retList,tubes[index-1])
+    addTubeData(retList,tubes[index])
     if index==(len(tubes)-1):
-        addTubeData(retList,birdPos,tubes[index])
+        addTubeData(retList,tubes[index])
         retList[len(tubes)-3]+=100
     else:
-        addTubeData(retList,birdPos,tubes[index+1])
+        addTubeData(retList,tubes[index+1])
 
     return retList
 
@@ -182,12 +182,16 @@ def playForTrainData(sess,flySimu):
 
     playCount=0
     liveCount=0
-    clickCount=1
+    clickCount=0
     while True:
-        if len(actions)>=1024*2:
+        if len(actions)>=1024:
             break
 
         playCount+=1
+
+        _actions=[]
+        _rawNewStates=[]
+        _rawOldStates=[]
         flySimu.reset()
         while True:
             oldState=flySimu.getState()
@@ -200,15 +204,24 @@ def playForTrainData(sess,flySimu):
                 action=[0,1]
             newState=flySimu.perform(dec)
 
-            actions.append(action)
-            rawNewStates.append(newState)
-            rawOldStates.append(oldState)
+            _actions.append(action)
+            _rawNewStates.append(newState)
+            _rawOldStates.append(oldState)
 
             if newState[0]==True:
                 liveCount+=flySimu.getLiveTime()
+                recordLength=40
+                if len(_actions)>=recordLength and random.uniform(0,1)<=0.5:
+                    actions=actions+_actions[-recordLength:]
+                    rawNewStates=rawNewStates+_rawNewStates[-recordLength:]
+                    rawOldStates=rawOldStates+_rawOldStates[-recordLength:]
+                else:
+                    actions = actions + _actions
+                    rawNewStates = rawNewStates + _rawNewStates
+                    rawOldStates = rawOldStates + _rawOldStates
                 break
 
-    greedyFactor=(200-liveCount/playCount)*0.0005
+    greedyFactor=(200-liveCount/playCount)*0.001
     if greedyFactor<0.01:
         greedyFactor=0.01
 
